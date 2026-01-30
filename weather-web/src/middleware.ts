a2f7ca984688641231eab143dd66f5434aa187bd
Ryota-Okumuraa <okumurahaku3298@icloud.com>
2026-01-31 03:53:48 +0900

    format

--- weather-web/src/middleware.ts
+++ weather-web/src/middleware.ts
@@ -31,9 +31,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|images|icons).*)",
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images|icons).*)"],
 };
 
 // こいつ動いてない
